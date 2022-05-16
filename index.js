61fc8b6f77080e09b1037ace758539de285d19c8
Franklin Benjamin <franciscovibes14@gmail.com>
2022-05-16 02:46:54 -0400

    commit tree c3a27ff62cb836feaee6aa054d5003fd0790cd9d

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name: "Franklin"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value
+    }
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+
+    return employee;
+    
+}
+
+function deleteFromEmployeeByKey(employee, key) { 
+    const copy =  {...employee}
+    delete copy[key]
+    return copy
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee
+}
